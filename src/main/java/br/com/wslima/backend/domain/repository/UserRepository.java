1d214b5127c62e6c44fd75bc86163873e264d460
Kennedy Lima <kennedy.wslima@gmail.com>
2025-06-21 17:17:11 -0400

    feature[user] implementado controller e repository

--- src/main/java/br/com/wslima/backend/domain/repository/UserRepository.java
+++ src/main/java/br/com/wslima/backend/domain/repository/UserRepository.java
@@ -3,5 +3,5 @@ package br.com.wslima.backend.domain.repository;
 import br.com.wslima.backend.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/br/com/wslima/backend/rest/controller/UserController.java
+++ src/main/java/br/com/wslima/backend/rest/controller/UserController.java
@@ -1,4 +1,15 @@
 package br.com.wslima.backend.rest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping
+    public String helloWord() {
+        return "Hello World";
+    }
 }
