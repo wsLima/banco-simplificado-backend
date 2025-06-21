8d586b71f1436b2e0a5743ab2ffd5e0e0aba7e82
Kennedy Lima <kennedy.wslima@gmail.com>
2025-06-21 16:22:23 -0400

    first commit

---
+++ src/main/java/br/com/wslima/backend/BancoSimplificadoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.wslima.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSimplificadoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoSimplificadoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/wslima/backend/BancoSimplificadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wslima.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSimplificadoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
